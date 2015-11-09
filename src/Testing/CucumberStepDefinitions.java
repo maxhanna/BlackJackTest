9b66ddebcaa451c76fa539584600197f3b832899
max@hannaconsultantgroup.com <max@hannaconsultantgroup.com>
2015-11-09 12:49:34 -0500

    Added Cucumber logging of timeouts

--- src/Testing/CucumberStepDefinitions.java
+++ src/Testing/CucumberStepDefinitions.java
@@ -6,6 +6,7 @@ import java.io.FileWriter;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 
 import cucumber.api.java.en.Given;
@@ -57,10 +58,36 @@ public class CucumberStepDefinitions {
 		}
 
 	}
+	public void logTimedOut(String func)
+	{
+
+		//LOG PROGRESS
+		File file = new File("ResultBJ.txt");
+
+
+		try {
+			FileWriter fileWritter;
+			BufferedWriter bufferWritter;
+			fileWritter = new FileWriter(file.getName(),true);
+			bufferWritter = new BufferedWriter(fileWritter);
+			bufferWritter.write(func+"() timed out. \r\n");
+			bufferWritter.close();
+			fileWritter.close();
+		} catch (Exception e) {
+			System.out.println("Could not open log file");
+		}
+
+	}
 	@Given("^I am on my Blackjack website$")
 	public void navigateToSite() throws Throwable {
 		try {
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 			driver.navigate().to("localhost:8000");
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("navigateToSite");
+			
 		}
 		catch (Exception E)
 		{
@@ -74,7 +101,14 @@ public class CucumberStepDefinitions {
 	@When("^I click on the username form$")
 	public void clickOnUserNameForm() throws Throwable {
 		try{
-			driver.findElement(By.id("username")).click();
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+			if (driver.findElement(By.id("username")).isDisplayed())
+				driver.findElement(By.id("username")).click();
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("clickOnUserNameForm");
+			
 		}
 		catch (Exception E) {
 			logFailed("clickOnUserNameForm");
@@ -87,7 +121,14 @@ public class CucumberStepDefinitions {
 	@When("^I populate the username form$")
 	public void populateUserNameForm() throws Throwable {
 		try{
-			driver.findElement(By.id("username")).sendKeys("Max Hanna");
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+			if (driver.findElement(By.id("username")).isDisplayed())
+				driver.findElement(By.id("username")).sendKeys("Max Hanna");
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("populateUserNameForm");
+			
 		}
 		catch (Exception E)
 		{
@@ -98,50 +139,76 @@ public class CucumberStepDefinitions {
 	}
 
 	@Then("^I should be on join game room page$")
-	public void checkOnUserNameConfirmationPage() throws Throwable {
+	public void goToGameTablePage() throws Throwable {
 		try {
-			driver.findElement(By.id("submit")).click();
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+			if (driver.findElement(By.id("submit")).isDisplayed())
+				driver.findElement(By.id("submit")).click();
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("goToGameTablePage");
+			
 		}
 		catch(Exception E) {
-			logFailed("checkOnUserNameConfirmationPage");
+			logFailed("goToGameTablePage");
 		}
 		if (!driver.findElement(By.id("submit")).isDisplayed())
 			System.exit(1);
 		//LOG PROGRESS
-		log("checkOnUserNameConfirmationPage");
+		log("goToGameTablePage");
 	}
 
 
 	@Given("^I am already logged in$")
-	public void I_have_logged_in() throws Throwable {
+	public void loggedIn() throws Throwable {
 		try {
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 			driver.navigate().to("localhost:8000/&username=Max Hanna");
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("loggedIn");
+			
 		}
 		catch (Exception e)
 		{
-			logFailed("I_have_logged_in");
+			logFailed("loggedIn");
 		}
 		//LOG PROGRESS
-		log("I_have_logged_in");
+		log("loggedIn");
 	}
 
 	@When("^I click on the join table button$")
-	public void I_click_on_the_join_table_button() throws Throwable {
+	public void clickOnJoinTable() throws Throwable {
 		try {
-			driver.findElement(By.id("submit")).click();	
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+			if (driver.findElement(By.id("submit")).isDisplayed())
+				driver.findElement(By.id("submit")).click();	
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("clickOnJoinTable");
+			
 		}
 		catch (Exception e)
 		{
-			logFailed("I_click_on_the_join_table_button");
+			logFailed("clickOnJoinTable");
 		}
 		//LOG PROGRESS
-		log("I_click_on_the_join_table_button");
+		log("clickOnJoinTable");
 	}
 
 	@Then("^I should be on game room page$")
 	public void inGameRoom() throws Throwable {
 		try {
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 			driver.navigate().to("http://localhost:8000/?join=Max Hanna&table=Carleton%20Room");
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("inGameRoom");
+			
 		}
 		catch(Exception e)
 		{
@@ -155,7 +222,13 @@ public class CucumberStepDefinitions {
 	@Given("^I am already in a game$")
 	public void iAmInGame() throws Throwable {
 		try {
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 			driver.navigate().to("http://localhost:8000/?join=Max Hanna&table=Carleton%20Room");
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("iAmInGame");
+			
 		}
 		catch(Exception e)
 		{
@@ -168,7 +241,14 @@ public class CucumberStepDefinitions {
 	@When("^I click on the hit button$")
 	public void iHit() throws Throwable {
 		try{
-			driver.findElement(By.id("hitButton")).click();	
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+			if (driver.findElement(By.id("hitButton")).isDisplayed())
+				driver.findElement(By.id("hitButton")).click();	
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("iHit");
+			
 		}
 		catch(Exception E)
 		{
@@ -181,7 +261,14 @@ public class CucumberStepDefinitions {
 	@When("^I click on the stay button$")
 	public void iStay() throws Throwable {
 		try {
-			driver.findElement(By.id("stayButton")).click();	
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+			if (driver.findElement(By.id("stayButton")).isDisplayed())
+				driver.findElement(By.id("stayButton")).click();	
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("iStay");
+			
 		}
 		catch (Exception e)
 		{
@@ -194,7 +281,13 @@ public class CucumberStepDefinitions {
 	@Then("^I should be on the end game page$")
 	public void endGamePage() throws Throwable {
 		try {
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 			driver.navigate().to("http://localhost:8000/?join=Max Hanna&table=Carleton%20Room");
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("endGamePage");
+			
 		}
 		catch (Exception e)
 		{
@@ -206,9 +299,17 @@ public class CucumberStepDefinitions {
 	@Given("^I populate the create table form$")
 	public void populateCreateTableForm() throws Throwable {
 		try{
-
-			driver.findElement(By.id("tableName")).sendKeys("Hungry");
-			driver.findElement(By.id("numAI")).sendKeys("3");
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+			if (driver.findElement(By.id("tableName")).isDisplayed() && driver.findElement(By.id("numAI")).isDisplayed())
+			{	
+				driver.findElement(By.id("tableName")).sendKeys("Hungry");
+				driver.findElement(By.id("numAI")).sendKeys("3");
+			}
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("populateCreateTableForm");
+			
 		}
 		catch (Exception E)
 		{
@@ -221,7 +322,14 @@ public class CucumberStepDefinitions {
 	@Given("^I click on the create table forms submit button$")
 	public void submitCreateTable() throws Throwable {
 		try{
-			driver.findElement(By.id("create")).click();	
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+			if (driver.findElement(By.id("create")).isDisplayed())
+				driver.findElement(By.id("create")).click();	
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("submitCreateTable");
+			
 		}
 		catch (Exception E)
 		{
@@ -233,7 +341,14 @@ public class CucumberStepDefinitions {
 	@When("^I click on the new join table button$")
 	public void joinNewTable() throws Throwable {
 		try {
-			driver.findElement(By.id("submit")).click();
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+			if (driver.findElement(By.id("submit")).isDisplayed())
+				driver.findElement(By.id("submit")).click();
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("joinNewTable");
+			
 		}
 		catch (Exception e)
 		{
@@ -246,7 +361,14 @@ public class CucumberStepDefinitions {
 	@Then("^I should be on new game room page$")
 	public void onNewGameRoomPage() throws Throwable {
 		try {
+			driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
 			driver.navigate().to("http://localhost:8000/?username=Max Hanna&table=Hungry");
+		}
+		catch (TimeoutException e)
+		{
+			logTimedOut("onNewGameRoomPage");
+			driver.navigate().to("http://localhost:8000/?username=Max Hanna&table=Hungry");
+			
 		}
 		catch (Exception e)
 		{
